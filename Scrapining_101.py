d7168e2a9e12042e04840d2ce32b9c66d445f48b
simbje <simbje@gmail.com>
2022-11-08 17:19:59 +0100

    jupyter

--- Scrapining_101.py
+++ Scrapining_101.py
@@ -5,12 +5,31 @@ Scraping in python test
 '''
 
 # prerequisites
-from bs4 import BeautifulSoup as bs
+from bs4 import BeautifulSoup as soup
 import requests
+import pandas as pd
 #
 
 url = "https://www.finn.no/realestate/homes/search.html?q=oslo&sort=PUBLISHED_DESC"
 
 result = requests.get(url)
-doc = bs(result.text, "html.parser")
+doc = soup(result.text, "html.parser")
 print(doc.prettify())
+
+
+## data some eg vil ha
+
+adresse = []
+beskrivelse = []
+pris= []
+total_pris = []
+
+bolig_data = soup.findAll('div', attrs= {'class':'p-16 grid f-grid sm:grid-cols-2'})
+
+print(bolig_data)
+
+
+
+
+
+
