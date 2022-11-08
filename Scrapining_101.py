6e6f89f20ecc817c1fbf44f7d5da57a85bd57d56
simbje <simbje@gmail.com>
2022-11-08 15:05:40 +0100

    weee

--- Scrapining_101.py
+++ Scrapining_101.py
@@ -6,8 +6,11 @@ Scraping in python test
 
 # prerequisites
 from bs4 import BeautifulSoup as bs
-
 import requests
 #
 
+url = "https://www.finn.no/realestate/homes/search.html?q=oslo&sort=PUBLISHED_DESC"
 
+result = requests.get(url)
+doc = bs(result.text, "html.parser")
+print(doc.prettify())
